e390f05d22f22d711c2946a184b56bbb8594c9f5
Chimo <chimo@chromic.org>
2017-03-22 00:32:26 +0000

    Implement pagination
    Closes #1

--- actions/filesadminpanel.php
+++ actions/filesadminpanel.php
@@ -12,7 +12,13 @@ class FilesadminpanelAction extends AdminPanelAction
     function prepare(array $args=array()) {
         parent::prepare($args);
 
-        $this->page = isset($args['page']) ? $args['page'] + 0 : 1;
+        $page = isset($args['page']) ? intval($args['page']) : 1;
+
+        if ($page <= 0) {
+            $page = 1;
+        }
+
+        $this->page = $page;
         $this->args = $args;
 
         $offset = ($this->page - 1) * 10;
@@ -84,6 +90,61 @@ class FilesadminpanelAction extends AdminPanelAction
 
         $this->elementEnd('tbody');
         $this->elementEnd('table');
+
+        $this->showPagination($this->page);
+    }
+
+    function showPagination($current_page) {
+        $have_before = false;
+        $have_after = false;
+
+        if ($current_page > 1) {
+            $have_before = true;
+        }
+
+        // FIXME: This might give us an empty last page if the total
+        //        amount of files is a multiple of 10
+        if (sizeof($this->files) === 10) {
+            $have_after = true;
+        }
+
+        $this->pagination($have_before, $have_after, $current_page, 'filesadminpanel');
+    }
+
+    /**
+     * This is a copy of Action::pagination because the 'Before'/'After' labels
+     * don't make sense and can't be overwritten...
+     */
+    function pagination($have_before, $have_after, $page, $action, $args=null)
+    {
+        // Does a little before-after block for next/prev page
+        if ($have_before || $have_after) {
+            $this->elementStart('ul', array('class' => 'nav',
+                                            'id' => 'pagination'));
+        }
+        if ($have_before) {
+            $pargs   = array('page' => $page-1);
+            $this->elementStart('li', array('class' => 'nav_prev'));
+            $this->element('a', array('href' => common_local_url($action, $args, $pargs),
+                                      'rel' => 'prev'),
+                           // TRANS: Pagination message to go to a page displaying information more in the
+                           // TRANS: present than the currently displayed information.
+                           _('Previous'));
+            $this->elementEnd('li');
+        }
+        if ($have_after) {
+            $pargs   = array('page' => $page + 1);
+            $this->elementStart('li', array('class' => 'nav_next'));
+            $this->element('a', array('href' => common_local_url($action, $args, $pargs),
+                                      'rel' => 'next'),
+                           // TRANS: Pagination message to go to a page displaying information more in the
+                           // TRANS: past than the currently displayed information.
+                           _('Next'));
+            $this->elementEnd('li');
+        }
+        if ($have_before || $have_after) {
+            $this->elementEnd('ul');
+        }
     }
 }
 
