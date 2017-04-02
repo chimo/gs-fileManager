a4c5c5f468743bc232e055fdde71107ce549a091
Chimo <chimo@chromic.org>
2017-04-02 04:15:28 -0400

    Add 'B' suffix for size < than 1 'K'

--- actions/filesadminpanel.php
+++ actions/filesadminpanel.php
@@ -80,7 +80,7 @@ class FilesadminpanelAction extends AdminPanelAction
     function formatBytes($size, $precision = 2)
     {
         $base = log($size, 1024);
-        $suffixes = array('', 'K', 'M', 'G', 'T');
+        $suffixes = array('B', 'K', 'M', 'G', 'T');
 
         return round(pow(1024, $base - floor($base)), $precision) .' '. $suffixes[floor($base)];
     }
